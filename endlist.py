db0e285ee1d3938b25288b4170472d66863340b8
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-07 23:00:39 +0530

    endlist

---
+++ endlist.py
@@ -0,0 +1,5 @@
+def list(a) :
+    list=[a[0],a[-1]]
+    return list
+a=[1,2,3,4,5,6,7,8]
+print list(a)
---
+++ prime.py
@@ -0,0 +1,8 @@
+def prime(a) :
+    if (a%a==0 and a%1==0) :
+        return "prime"
+    else :
+        return "not"
+a=input("enter the number")
+print (prime(a))
+
