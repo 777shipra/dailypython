0731b61da00500fe2ffbc81e15db0e587b2fbbd2
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-08 18:59:58 +0530

    fibnocci series

--- prime.py
+++ prime.py
@@ -1,7 +1,8 @@
 def prime(a) :
+    i=range(2,100)
     if (a%a==0 and a%1==0) :
         return "prime"
-    else :
+    elif a%i==0 :
         return "not"
 a=input("enter the number")
 print (prime(a))
