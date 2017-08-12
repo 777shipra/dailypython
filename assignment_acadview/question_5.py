61bcf34d6eb24a3b3b98d7273efdeab673d3ca50
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-12 11:01:00 +0530

    assignment1

--- assignment_acadview/question_5.py
+++ assignment_acadview/question_5.py
@@ -1,6 +1,8 @@
 my_str = raw_input("enter your sentence")
 words = my_str.split()
 words.sort()
+list=[]
 print("The sorted words are:")
 for word in words:
-   print(word)
+   list.append(str(word))
+print ','.join(list)
