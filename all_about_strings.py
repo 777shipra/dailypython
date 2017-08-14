c34b579aa1adf1a5e9314f17c4664ff60fa6cd45
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-14 20:03:15 +0530

    some string behaviors

---
+++ all_about_strings.py
@@ -0,0 +1,20 @@
+str='hello world'
+#accessing characters in string
+print str
+print str[0]
+print str[2:5]
+print str[2:]
+print str*2
+print str + 'TEST'
+print str[:7]
+print str[::-1]
+print str[::]
+print str[:2:-1]
+#operations in string
+list_enumerate=list(enumerate(str))
+print('list(enumerate(str) = ', list_enumerate)
+print len(str)
+del str #delete the whole string
+
+
+
