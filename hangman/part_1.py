eb77c035c8331be4a33740a581f1c1b010559db7
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-22 19:15:13 +0530

    hangman

--- hangman/part_1.py
+++ hangman/part_1.py
@@ -2,17 +2,26 @@ import random
 line = random.choice(open('sowpods.txt').readlines())
 print line
 count=0
+word=[]
+guess=[]
+word.append(line)
 print "<----------welcome to HANGMAN----------->"
 print "guess the word , BEWARE you just got 6 chances \n LET'S GO"
-for char in line:
-    print "-",
+for w in word:
+    for char in w:
+     guess.append("-")
+print guess
+your_guess=[]
+a=raw_input("enter your character:")
+for w in word:
+    for char in w:
+        if a==char:
+            guess.append(char)
+        else:
+            guess.append("-")
+print guess
 
-guess=raw_input("enter your character:")
-for char in line:
-            if guess == char:
-                print char,
-            else:
-                print "-",
+
 
 
 
