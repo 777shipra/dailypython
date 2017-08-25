f7196d29b45878a255c7b20bc0bb5cc7df5cb6fe
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-25 11:25:47 +0530

    about enumerate

---
+++ enumerate.py
@@ -0,0 +1,6 @@
+choices=['pizza','ice','nachos','trigger']
+for i,item in enumerate(choices):
+    print i,choices #prints the whole list
+    print i,item #prints the content of the list
+for i,items in enumerate (choices,1):
+    print i,items #begins the itteration from index 1
