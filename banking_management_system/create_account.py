4f32fd34e789b0f4b40c291649acd39de95d724e
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-26 18:49:38 +0530

    in process

--- banking_management_system/create_account.py
+++ banking_management_system/create_account.py
@@ -1,6 +1,19 @@
 def create_account():
-    acc_no=int(raw_input("enter the account number:"))
-    acc_name=raw_input("enter the name of the account holder:")
-    acc_type=raw_input("enter the type of account (C/S):")
-    acc_amount=int(raw_input("enter the amount \n (NOTE:amount>=500 for S and amount>=1000 for C): "))
-    print "<-----ACCOUNT CREATED----->"
+    print "<--------WELCOME , CREATE YOUR ACCOUNT------->"
+    acc_no=(raw_input("enter the account number:"))
+    if (len(acc_no)>0 and len(acc_no)==10):
+             acc_name=raw_input("enter the name of the account holder:")
+    else:
+            print "<---INVALID ACCOUNT NUMBER,PLEASE TRY AGAIN--->"
+    while(len(acc_no)>0 and len(acc_no)==10):
+        acc_type=raw_input("enter the type of account (C/S):")
+        if (len(acc_type)==1 and acc_type=="c" or acc_type=="C" or acc_type=="S"or acc_type=="s"):
+            acc_amount=int(raw_input("enter the amount \n (NOTE:amount>=500 for S and amount>=1000 for C): "))
+            if ((acc_type=="s"or acc_type=="S") and acc_amount>=500) or ((acc_type=="c" or acc_type=="C") and acc_type>=1000):
+                print "<-----ACCOUNT CREATED----->"
+            else:
+                print "<-----NOT CORRECT AMOUNT----->"
+        else:
+            print "<-----INVALID ACCOUNT TYPE ,TYPE AGAIN (C=current/S=saving)"
+
+
--- banking_management_system/main.py
+++ banking_management_system/main.py
@@ -1,8 +1,9 @@
+from create_account import create_account
 print "Welcome to BANKING MANAGEMENT SYSTEM \n <---------------MAIN MENU--------------->"
 print "\t 1:NEW ACCOUNT \n \t 2:DEPOSIT AMOUNT \n\t 3:WITHDRAW AMOUNT \n\t 4:BALANCE ENQUIRY \n\t 5:ALL ACCOUNT HOLDER LIST \n\t 6:CLOSE AN ACCOUNT \n\t 7:MODIFY AN ACCOUNT \n\t 8:EXIT"
 choice=int(raw_input("Make your choice (1-8):"))
 if choice==1:
-   print "code"
+   create_account()
 elif choice==2:
    print "code"
 elif choice==3:
