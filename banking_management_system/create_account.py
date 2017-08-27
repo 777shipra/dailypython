2f654d6d30a51ac195985395d703d9b8f0c035c7
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-27 19:26:37 +0530

    inprocess

--- banking_management_system/create_account.py
+++ banking_management_system/create_account.py
@@ -1,16 +1,17 @@
 def create_account():
     print "<--------WELCOME , CREATE YOUR ACCOUNT------->"
-    acc_no=(raw_input("enter the account number:"))
-    if (len(acc_no)>0 and len(acc_no)==10):
-             acc_name=raw_input("enter the name of the account holder:")
+    dict['acc_no']=raw_input("enter the account number:")
+    if (len(dict['acc_no'])>0 and len(dict['acc_no'])==10):
+             dict['acc_name']=raw_input("enter the name of the account holder:")
     else:
             print "<---INVALID ACCOUNT NUMBER,PLEASE TRY AGAIN--->"
-    while(len(acc_no)>0 and len(acc_no)==10):
+    while(len(dict['acc_no'])>0 and len(dict['acc_no'])==10):
         acc_type=raw_input("enter the type of account (C/S):")
-        if (len(acc_type)==1 and acc_type=="c" or acc_type=="C" or acc_type=="S"or acc_type=="s"):
-            acc_amount=int(raw_input("enter the amount \n (NOTE:amount>=500 for S and amount>=1000 for C): "))
-            if ((acc_type=="s"or acc_type=="S") and acc_amount>=500) or ((acc_type=="c" or acc_type=="C") and acc_type>=1000):
+        if (len(dict['acc_type'])==1 and dict['acc_type']=="c" or dict['acc_type']=="C" or dict['acc_type']=="S"or dict['acc_type']=="s"):
+            dict['acc_amount']=int(raw_input("enter the amount \n (NOTE:amount>=500 for S and amount>=1000 for C): "))
+            if ((dict['acc_type']=="s"or dict['acc_type']=="S") and dict['acc_amount']>=500) or ((dict['acc_type']=="c" or dict['acc_type']=="C") and dict['acc_amount']>=1000):
                 print "<-----ACCOUNT CREATED----->"
+
             else:
                 print "<-----NOT CORRECT AMOUNT----->"
         else:
---
+++ banking_management_system/details.py
@@ -0,0 +1,9 @@
+import create_account
+record=[]
+record.append({
+    "account number":dict['acc_no'],
+    "holder name":dict['acc_name'],
+    "account type":dict['acc_type'],
+    "account amount":dict['acc_amount']
+})
+print record
