064309a51e0ff92524a5b967d9fa651613bd9053
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-29 21:04:31 +0530

    inprocess

--- banking_management_system/trial2.py
+++ banking_management_system/trial2.py
@@ -10,6 +10,7 @@ list=[{
     "acc type":"c",
     "acc amount":100
 }]
+#<-------------------------------------DEPOSIT AMOUNT-------------------------------------------------->
 a=int(raw_input("enter the acc no:"))
 b=int(raw_input("enter the amount :"))
 for dict in list:
@@ -20,6 +21,7 @@ for dict in list:
             print "after deposit amount is :",dict['acc amount']
             break
 #<----------------------------------------WITHDRAW AMOUNT----------------------------------------------------->
+
 acc_no=int(raw_input("enter the account number:"))
 acc_amount=int(raw_input("enter the amount to  withdraw:"))
 for dict in list:
@@ -28,7 +30,22 @@ for dict in list:
             if dict['acc amount']>acc_amount:
                 dict['acc amount']=dict['acc amount']-acc_amount
                 print "amount after withdraw :",dict['acc amount']
+                break
             else:
                 print "amount is less in account"
+                break
         else:
             print "INVALID ACCOUNT NUMBER"
+            break
+#<------------------------------------------BALANCE ENQUIRY-------------------------------------------------->
+acc_no=int(raw_input("enter the account number:"))
+for dict in list:
+    for keys,values in dict.iteritems():
+        if dict['acc.no']==acc_no:
+            print "account number of the holder:",dict['acc.no']
+            print "balance in the account is :",dict['acc amount']
+            exit()
+        else:
+            print "INVALID ACCOUNT NUMBER"
+
+
