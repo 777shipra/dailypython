8ae2322ef2512c555eb28187f1e5200eb60a4721
shipra chauhan <777chauhanshipra@gmail.com>
2017-08-30 19:11:50 +0530

    inprocess

--- banking_management_system/modify_trial.py
+++ banking_management_system/modify_trial.py
@@ -3,10 +3,18 @@ acc_no=int(raw_input("enter the account number:"))
 for dict in list:
     for keys,values in dict.iteritems():
         if dict['acc.no']==acc_no:
-            print dict
+            print dict.values()
             new_acc_name=raw_input("enter the name:")
-            dict['acc. name']=new_acc_name
+            if len(new_acc_name)>0:
+               dict['acc. name']=new_acc_name
+            else:
+                print "<----------INVALID NAME----------->"
+
             new_acc_type=raw_input("enter the type of account:")
-            dict['acc type']=new_acc_type
+            if (len(dict['acc type']) == 1 and dict['acc type'] == "c" or dict['acc type'] == "C" or dict[ 'acc type'] == "S" or dict['acc type'] == "s"):
+
+                dict['acc type']=new_acc_type
+            else:
+                print "<------------WRONG ACCOUNT TYPE--------->"
             print dict
             break
